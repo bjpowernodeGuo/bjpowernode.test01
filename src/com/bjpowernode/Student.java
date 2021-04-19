49a001779c6409d91c36b2cefd57d0832c02eb42
zs <zs@bjpowernode.com>
2021-04-19 16:28:18 +0800

    liujian

--- src/com/bjpowernode/Student.java
+++ src/com/bjpowernode/Student.java
@@ -2,7 +2,7 @@ package com.bjpowernode;
 
 public class Student {
     private String name;
-
+    private String address;
     @Override
     public String toString() {
         return "Student{" +
